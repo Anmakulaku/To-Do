276c00fd14529d237051c2109d11ddad28f9d50c
Anmakulaku <anmakulaku@gmail.com>
2023-11-29 14:19:37 +0100

    First commit To-Do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header section padding'>
+        <div className="header__content">
+            <div className='header__gradient-big'></div>
+            <div className='header__gradient-small'></div>
+            <h1 className="title">DO IT NOW</h1>
+        </div>
+      </div>
     </div>
   );
 }
