0fe24f27dc0d569152e1497a788c626a7c0d0263
Anmakulaku <anmakulaku@gmail.com>
2023-11-30 15:55:02 +0100

    Add Edit, Delete & Done

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Todo } from '../interface';
 import { AiOutlineEdit, AiOutlineDelete,  } from 'react-icons/ai'
 import { MdOutlineDownloadDone  } from 'react-icons/md'
@@ -11,24 +11,65 @@ type TDIProps = {
 }
 
 const ToDoItem: React.FC<TDIProps> = ({todo, todos, setTodos}) => {
-    return <form className='toDoItem'>
-        <div className="toDoItem__content">
-            <span className="toDoItem__text">
-                {todo.todo}
-            </span>
-            <div className='toDoItem__icons'>
-                <div className="toDoItem__icon">
-                    <AiOutlineEdit />
-                </div>
-                <div className="toDoItem__icon">
-                    <AiOutlineDelete />
-                </div>
-                <div className="toDoItem__icon">
-                    <MdOutlineDownloadDone />
+
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+    const handleDone = (id: number) => {
+        setTodos(
+            todos.map((todo)=> 
+            todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+            ))
+    };
+
+    const handleDelete = (id: number) => {
+        setTodos(
+            todos.filter((todo)=> 
+            todo.id !== id
+            ))
+    };
+
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTodos(todos.map((todo) => (
+            todo.id === id ? {...todo, todo: editTodo} : todo
+        )));
+        setEdit(false);
+    }
+
+    return (
+    
+        <form className='toDoItem' onSubmit={(e) => handleEdit(e,todo.id)}>
+            <div className="toDoItem__content">
+                {edit ? (
+                    <input 
+                    value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className='toDoItem__input'/>
+                ): todo.isDone ? (
+                    <s className="toDoItem__text">{todo.todo}</s>
+                ) : (
+                    <span className="toDoItem__text">{todo.todo}</span>
+                )}
+                
+                <div className='toDoItem__icons'>
+                    <div className="toDoItem__icon" 
+                        onClick={() => {
+                            if(!edit && !todo.isDone) {
+                                setEdit(!edit);
+                            } 
+                        }}
+                    >
+                        <AiOutlineEdit />
+                    </div>
+                    <div className="toDoItem__icon" onClick={()=>handleDelete(todo.id)}>
+                        <AiOutlineDelete />
+                    </div>
+                    <div className="toDoItem__icon" onClick={()=>handleDone(todo.id)}>
+                        <MdOutlineDownloadDone />
+                    </div>
                 </div>
             </div>
-        </div>
-    </form>
+        </form>
+    );
 }
 
 export default ToDoItem
