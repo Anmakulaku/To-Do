0da73567d0aa3b98c254eaf7da76cd553225b92a
Anmakulaku <anmakulaku@gmail.com>
2023-12-01 15:06:20 +0100

    Add classes &  styles for tasks d&d

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,10 +11,25 @@ interface TDListProps {
 const TodoList: React.FC<TDListProps> = ({todos, setTodos}) => {
     return (
         <div className='todoList'>
-            <div className="todoList__content">
-                {todos.map((todo: Todo) => (
-                    <ToDoItem todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
-                ))}
+            <div className="todoList__container">
+                <div className='todoList__content todoList__todos'>
+                    <div className='todoList__title'>
+                        <h2 className='todoList__todosTitle'>Tasks to do:</h2>
+                    </div>
+                    {
+                        todos.map((todo: Todo) => (
+                        <ToDoItem todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+                    ))}
+                </div>
+                <div className='todoList__content todoList__emergency'>
+                    <div className='todoList__title'>
+                            <h2 className='todoList__emergencyTitle'>Do it right now!</h2>
+                    </div>
+                    {
+                        todos.map((todo: Todo) => (
+                        <ToDoItem todo={todo} key={todo.id} todos={todos} setTodos={setTodos} />
+                    ))}
+                </div>
             </div>
         </div>
     )
